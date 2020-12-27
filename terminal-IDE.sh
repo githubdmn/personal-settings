7c74734a90c2ed4a4cf4252b03b1ada619600b85
githubdmn <dmninbox@gmail.com>
2020-12-27 20:25:05 +0100

    programming-2020

---
+++ terminal-IDE.sh
@@ -0,0 +1,7 @@
+xfce4-terminal --maximize \
+--tab --title=js-2020 --working-directory=$HOME/Public/programming-2020/js-2020 \
+--tab --title=ts-2020 --working-directory=$HOME/Public/programming-2020/ts-2020 \
+--tab --title=nodejs-2020 --working-directory=$HOME/Public/programming-2020/nodejs-2020 \
+--tab --title=nestjs-2020 --working-directory=$HOME/Public/programming-2020/nestjs-2020 \
+--tab --title=py-2020 --working-directory=$HOME/Public/programming-2020/py-2020 \
+--tab --title=home --working-directory=$HOME 
